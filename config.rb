03aa987be7b8f598fdc22869bcd303a0b3f056b1
Rishabh Rao <rishabhsrao@yahoo.com>
2013-02-24 09:44:03 +0530

    Added foundation compass plugin.

---
+++ config.rb
@@ -0,0 +1,8 @@
+# Foundation
+require "zurb-foundation"
+
+# Local development paths
+sass_dir = "sass"
+images_dir = "source/images"
+javascripts_dir = "source/javascripts"
+fonts_dir = "source/fonts"
